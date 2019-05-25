377ac3835cae3614df0ebc1a9a31f888edeba211
lupeng06 <1535933394@qq.com>
2019-05-25 16:19:11 +0800

    添加 tag 接口

--- models/tag.go
+++ models/tag.go
@@ -17,3 +17,23 @@ func GetTags(pageNum,pageSize int,maps interface{}) (tags []Tag)  {
 	db.Debug().Where(maps).Offset(pageNum).Limit(pageSize).Find(&tags)
 	return
 }
+
+
+func ExitsByName(name string) bool {
+	var tag  Tag
+	db.Where("name=?",name).First(&tag)
+	if tag.ID > 0  {
+		return  true
+	}
+	return false
+}
+
+func AddTag(name,creatdBy string,state int) bool {
+
+	db.Create(&Tag{
+		Name:name,
+		CreatedBy:creatdBy,
+		State:state,
+	})
+	return true
+}
--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -6,6 +6,7 @@ import (
 	"ginblog/pkg/setting"
 	"ginblog/pkg/util"
 	"github.com/Unknwon/com"
+	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -35,6 +36,33 @@ func GetTags(c *gin.Context)  {
 
 
 func AddTag(c *gin.Context)  {
+	name := c.Query("name")
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
+	creatdBy := c.Query("creatd_by")
+
+	valid := validation.Validation{}
+
+	valid.Required(name,"name").Message("名字不能为空")
+	valid.MaxSize(name,100,"name").Message("长度不能操作100个字符")
+	valid.Required(creatdBy,"creatd_by").Message("创建人不能为空")
+	valid.Range(state,0,1,"state").Message("state 只能是 0或者1")
+
+	code := e.INVALID_PARAMS
+
+	if !valid.HasErrors() {
+		if !models.ExitsByName(name) {
+			code = e.SUCCESS
+			models.AddTag(name,creatdBy,state)
+		}else {
+			code = e.ERROR_EXIST_TAG
+		}
+	}
+
+	c.JSON(http.StatusOK,gin.H{
+		"code":code,
+		"msg":"",
+		"data":make(map[string]string),
+	})
 }
 
 func EditTag(c *gin.Context)  {
@@ -42,3 +70,5 @@ func EditTag(c *gin.Context)  {
 
 func DeleteTag(c *gin.Context)  {
 }
+
+
